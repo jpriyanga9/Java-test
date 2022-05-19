98dd2be13105c630c6551b621810b6b20fac297e
91948 <91948@LAPTOP-0A5E04UV>
2022-05-19 12:24:53 -0400

    First commit

---
+++ src/Food.java
@@ -0,0 +1,22 @@
+
+public class Food {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+		displayQuestion();
+		System.out.println("Pizza! and");
+		
+		System.out.println("Hamburger! and");
+		
+		System.out.println("Diet Pepsi!");
+
+		System.out.println("Now that’s what I am talking about ......");
+		}
+	}
+		public static void displayQuestion()
+		{
+		System.out.println("What does Michael like?");
+	}
+
+}
---
+++ src/coursesoftware/CourseSoftware.java
@@ -0,0 +1,23 @@
+package coursesoftware;
+
+public class CourseSoftware {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		{
+		displayQuestion();
+		System.out.println("Windows Laptop ");
+		displayQuestion();
+		System.out.println("Mac Laptop ");
+		displayQuestion();
+		System.out.println("Java software");
+		displayQuestion();
+		System.out.println("Eclipse software");
+		}
+	}
+		public static void displayQuestion()
+		{
+		System.out.println("What software are you using in Class?");
+		}
+			
+
+}
